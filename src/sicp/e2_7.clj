948f786c645bdbfef6c4fc9416ea7b7475c6a109
hifistar <yenshan@gk9.so-net.ne.jp>
2017-08-05 16:31:01 +0900

    add e2_7.clj

---
+++ src/sicp/e2_7.clj
@@ -0,0 +1,34 @@
+(ns sicp.e2_7)
+
+;;
+;; 2.1.4
+;;
+(defn make-interval [a b] (list a b))
+(defn lower-bound [x] (first x))
+(defn upper-bound [x] (last x))
+
+(defn add-interval [x y]
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(defn mul-interval [x y]
+  (let [p1 (* (lower-bound x) (lower-bound y))
+        p2 (* (lower-bound x) (upper-bound y))
+        p3 (* (upper-bound x) (lower-bound y))
+        p4 (* (upper-bound y) (upper-bound y))]
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(defn div-interval [x y]
+  (mul-interval
+                x
+                (make-interval (/ 1.0 (lower-bound y))
+                               (/ 1.0 (upper-bound y)))))
+
+
+(def a (make-interval 10 12))
+(def b (make-interval 13 15))
+
+(add-interval a b)
+(mul-interval a b)
+(div-interval a b)
