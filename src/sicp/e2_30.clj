b7de32a3742e2665709ba206a6c354b432c855fd
hifistar <yenshan@gk9.so-net.ne.jp>
2017-08-11 20:58:57 +0900

    add e2_30

---
+++ src/sicp/e2_30.clj
@@ -0,0 +1,31 @@
+(ns sicp.e2_30)
+
+(defn square [x] (* x x))
+
+;;
+;; definition of square-tree without using map
+;;
+(defn square-tree [tree]
+  (cond (not (coll? tree)) (square tree)
+        (empty? tree) nil
+        :else (cons (square-tree (first tree))
+                    (square-tree (rest tree)))))
+
+;;
+;; definition of square-tree by using map
+;;
+(defn square-tree_map [tree]
+  (map (fn [subtree]
+         (if (coll? subtree) 
+           (square-tree_map subtree)
+           (square subtree)))
+       tree))
+
+(def ttree (list 1 
+                 (list 2 (list 3 4) 5)
+                 (list 6 7)))
+
+(defn e2_30-test []
+  (println "original tree:" ttree)
+  (println "square tree 1:" (square-tree ttree))
+  (println "square tree 2:" (square-tree_map ttree)))
