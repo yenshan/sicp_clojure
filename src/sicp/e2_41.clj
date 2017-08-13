aa31a8c727b840b13662650e85d695e893c1e173
hifistar <yenshan@gk9.so-net.ne.jp>
2017-08-13 21:37:18 +0900

    add comments of make-n-tuples

--- src/sicp/e2_41.clj
+++ src/sicp/e2_41.clj
@@ -1,13 +1,18 @@
 (ns sicp.e2_41
   (:require [sicp.nested-mapping :refer [flatmap]]))
 
-(defn make-n-tuples [n coll]
+(defn make-n-tuples
+  "make all of ordered n-tuples from a collection of integers."
+  [n coll]
   (cond (= n 1) (map #(list %) coll)
         (empty? coll) nil 
         :else (flatmap (fn [x]
                          (map (fn [a] (cons x a))
                               (make-n-tuples (dec n) (remove #(= x %) coll))))
                        coll)))
+
+(make-n-tuples 2 '(1 2))
+;-> ((1 2) (2 1))
 
 (defn find-ordered-triples-sum [n s]
   (filter (fn [[a b c]] (= (+ a b c) s))
