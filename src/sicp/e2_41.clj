72ad7e8d50ee446c9788df01f588c20e4375e169
hifistar <yenshan@gk9.so-net.ne.jp>
2017-08-15 06:30:39 +0900

    add variation of implementation.

--- src/sicp/e2_41.clj
+++ src/sicp/e2_41.clj
@@ -2,7 +2,8 @@
   (:require [sicp.nested-mapping :refer [flatmap]]))
 
 ;;
-;; implement exercise 2.41 in SICP style
+;; implement exercise 2.41, 
+;;   At first, create general purpose function for making n-tuple, then use it.
 ;;
 (defn make-n-tuples
   "make all of ordered n-tuples from a collection of integers."
@@ -37,3 +38,38 @@
                 (cons x a))))
 
 (make-n-tuples2 2 '(1 2))
+
+;;
+;; --------------------------------------------------------------------
+;;
+
+;;
+;; implement exercise 2.41 function simply by using three map loops and filter
+;;
+(defn find-ordered-triples-sum2 [n s]
+  (filter (fn [[a b c]] 
+            (and (not= a b) (not= b c) (not= a c)
+                 (= s (+ a b c))))
+          (flatmap 
+            (fn [a] (flatmap 
+                      (fn [b] (map 
+                                (fn [c] (list a b c))
+                                (range 1 (inc n))))
+                      (range 1 (inc n))))
+            (range 1 (inc n)))))
+
+(find-ordered-triples-sum2 6 6)
+
+
+;;
+;; impelent exercise 2.41 function by using for-macro with when
+;;
+(defn clj-find-ordered-triples-sum [n s]
+  (for [a (range 1 (inc n))
+        b (range 1 (inc n))
+        c (range 1 (inc n))
+        :when (and (not= a b) (not= b c) (not= a c))
+        :when (= s (+ a b c))]
+    (list a b c)))
+
+(clj-find-ordered-triples-sum 6 6)
