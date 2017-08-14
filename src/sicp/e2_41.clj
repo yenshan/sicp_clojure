64cd66be40fd4c1bf9034ab83b7a90d9d2206466
hifistar <yenshan@gk9.so-net.ne.jp>
2017-08-14 22:46:24 +0900

    add implementation of make-n-tuples by using for-macro

--- src/sicp/e2_41.clj
+++ src/sicp/e2_41.clj
@@ -1,15 +1,19 @@
 (ns sicp.e2_41
   (:require [sicp.nested-mapping :refer [flatmap]]))
 
+;;
+;; implement exercise 2.41 in SICP style
+;;
 (defn make-n-tuples
   "make all of ordered n-tuples from a collection of integers."
   [n coll]
   (cond (= n 1) (map #(list %) coll)
         (empty? coll) nil 
-        :else (flatmap (fn [x]
-                         (map (fn [a] (cons x a))
-                              (make-n-tuples (dec n) (remove #(= x %) coll))))
-                       coll)))
+        :else (flatmap
+                (fn [x]
+                  (map (fn [a] (cons x a))
+                       (make-n-tuples (dec n) (remove #(= x %) coll))))
+                coll)))
 
 (make-n-tuples 2 '(1 2))
 ;-> ((1 2) (2 1))
@@ -20,3 +24,16 @@
 
 
 (find-ordered-triples-sum 6 6)
+
+;;
+;; implement make-n-tuples by using for-macro
+;;
+(defn make-n-tuples2
+  [n coll]
+  (cond (= n 1) (map #(list %) coll)
+        (empty? coll) nil 
+        :else (for [x coll
+                    a (make-n-tuples2 (dec n) (remove #(= x %) coll))]
+                (cons x a))))
+
+(make-n-tuples2 2 '(1 2))
